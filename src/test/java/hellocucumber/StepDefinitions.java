7b5211815a94ee4b248cb22f4edd175feb045b18
German <german.a.almonte@gmail.com>
2025-02-24 21:12:29 -0500

    first commit

--- src/test/java/hellocucumber/StepDefinitions.java
+++ src/test/java/hellocucumber/StepDefinitions.java
@@ -1,21 +1,35 @@
 package hellocucumber;
 
-import io.cucumber.java.en.*;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
+import static org.assertj.core.api.Assertions.assertThat;
 
-import static org.junit.jupiter.api.Assertions.*;
+class IsItFriday {
+    static String isItFriday(String today) {
+        return "Friday".equals(today) ? "TGIF" : "Nope" ;
+    }
+}
 
 public class StepDefinitions {
+    private String today;
+    private String actualAnswer;
 
-    @Given("an example scenario")
-    public void anExampleScenario() {
+    @Given("today is Sunday")
+    public void today_is_Sunday() {
+        today = "Sunday";
+    }
+    @Given("today is Friday")
+    public void today_is_Friday() {
+        today = "Friday";
+    }
+    @When("I ask whether it's Friday yet")
+    public void i_ask_whether_it_s_Friday_yet() {
+        actualAnswer = IsItFriday.isItFriday(today);
     }
 
-    @When("all step definitions are implemented")
-    public void allStepDefinitionsAreImplemented() {
+    @Then("I should be told {string}")
+    public void i_should_be_told(String expectedAnswer) {
+        assertThat(actualAnswer).isEqualTo(expectedAnswer);
     }
-
-    @Then("the scenario passes")
-    public void theScenarioPasses() {
-    }
-
 }
